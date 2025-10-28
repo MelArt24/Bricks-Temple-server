b1c0a01c8b34843f802454fa7888c85518fa8efb
MelArt24 <animator2404@gmail.com>
2025-10-28 20:40:04 +0200

    Initial commit - Brick's Temple Server setup

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    kotlin("jvm") version "2.0.20"
+    id("io.ktor.plugin") version "3.0.0"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+application {
+    mainClass.set("ApplicationKt")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    val ktorVersion = "3.0.0"
+    val logbackVersion = "1.5.6"
+    val exposedVersion = "0.53.0"
+    val postgresVersion = "42.7.2"
+
+    // Ktor server core
+    implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
+
+    // Content negotiation + JSON serialization
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktorVersion")
+
+    // Logging
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+
+    // Database (Exposed ORM + PostgreSQL)
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    implementation("org.postgresql:postgresql:$postgresVersion")
+
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,5 @@
+package com.brickstemple
+
+fun main() {
+
+}
