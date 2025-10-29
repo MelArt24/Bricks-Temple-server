56ad8f3cd9f198796c3afe4e3c40a93bcf5f0acb
MelArt24 <animator2404@gmail.com>
2025-10-29 20:30:35 +0200

    comment useless for Render code

--- src/main/kotlin/DatabaseFactory.kt
+++ src/main/kotlin/DatabaseFactory.kt
@@ -10,24 +10,36 @@ import io.github.cdimascio.dotenv.dotenv
 
 fun Application.configureDatabase() {
 
-    val dotenv = dotenv()
-    val dbUrl = dotenv["DB_URL"]
-    val dbUser = dotenv["DB_USER"]
-    val dbPassword = dotenv["DB_PASSWORD"]
+//    val dotenv = dotenv()
+//    val dbUrl = dotenv["DB_URL"]
+//    val dbUser = dotenv["DB_USER"]
+//    val dbPassword = dotenv["DB_PASSWORD"]
+//
+//    Database.connect(
+//        url = dbUrl ?: error("DB_URL not found"),
+//        driver = "org.postgresql.Driver",
+//        user = dbUser ?: error("DB_USER not found"),
+//        password = dbPassword ?: error("DB_PASSWORD not found")
+//    )
 
+    val config = environment.config.config("ktor.database")
     Database.connect(
-        url = dbUrl ?: error("DB_URL not found"),
-        driver = "org.postgresql.Driver",
-        user = dbUser ?: error("DB_USER not found"),
-        password = dbPassword ?: error("DB_PASSWORD not found")
+        url = config.property("url").getString(),
+        driver = config.property("driver").getString(),
+        user = config.property("user").getString(),
+        password = config.property("password").getString()
     )
 
 
     TransactionManager.manager.defaultIsolationLevel = Connection.TRANSACTION_SERIALIZABLE
 
     transaction {
-        // we will create tables here
+//        exec("SELECT 1;") { rs ->
+//            if (rs.next()) {
+//                println("✅✅✅ Database test query succeeded!")
+//            }
+//        }
     }
 
-    log.info("Connected to PostgreSQL database!")
+//    log.info("✅✅✅ Connected to PostgreSQL database!")
 }
