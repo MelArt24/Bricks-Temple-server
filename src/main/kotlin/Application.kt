2b411132dddf9149f730fdbed1789444bee5586b
MelArt24 <animator2404@gmail.com>
2025-10-29 10:28:05 +0200

    feat: add application.conf

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,5 +1,14 @@
 package com.brickstemple
 
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.application.*
+
 fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
 
 }
