b9fd5c9af512051230b0ab0812607b988c495b27
MelArt24 <animator2404@gmail.com>
2025-11-05 14:57:47 +0200

    test: update ProductRoutesErrorTest

--- src/test/kotlin/ProductRoutesErrorTest.kt
+++ src/test/kotlin/ProductRoutesErrorTest.kt
@@ -1,6 +1,10 @@
 package com.brickstemple
 
+import TokenResponse
+import com.brickstemple.dto.users.UserDto
 import com.brickstemple.fakeRepositories.FakeProductRepository
+import com.brickstemple.fakeRepositories.FakeUserRepository
+import com.brickstemple.plugins.configureSecurity
 import com.brickstemple.plugins.configureSerialization
 import com.brickstemple.routes.productRoutes
 import io.ktor.client.request.*
@@ -9,13 +13,29 @@ import io.ktor.http.*
 import io.ktor.server.testing.*
 import kotlin.test.*
 import io.ktor.server.routing.*
+import com.brickstemple.routes.authRoutes
+
+import io.ktor.client.*
+
 
 class ProductRoutesErrorTest {
+
+    private suspend fun login(client: HttpClient, email: String, password: String): String {
+        val response = client.post("/auth/login") {
+            contentType(ContentType.Application.Json)
+            setBody("""{"email": "$email", "password": "$password"}""")
+        }
+        assertEquals(HttpStatusCode.OK, response.status)
+        val json = response.bodyAsText()
+        val tokenResponse = kotlinx.serialization.json.Json.decodeFromString<TokenResponse>(json)
+        return tokenResponse.token.trim()
+    }
 
     @Test
     fun `GET non-existent product returns 404`() = testApplication {
         application {
             configureSerialization()
+            configureSecurity()
             routing { productRoutes(FakeProductRepository()) }
         }
 
@@ -27,6 +47,7 @@ class ProductRoutesErrorTest {
     fun `GET invalid id returns 400`() = testApplication {
         application {
             configureSerialization()
+            configureSecurity()
             routing { productRoutes(FakeProductRepository()) }
         }
 
@@ -37,12 +58,27 @@ class ProductRoutesErrorTest {
 
     @Test
     fun `DELETE non-existent product returns 404`() = testApplication {
+        val userRepo = FakeUserRepository()
+        val productRepo = FakeProductRepository()
+
+        userRepo.create(UserDto(username = "admin", email = "admin@mail.com", password = "123456", role = "admin"))
+
         application {
             configureSerialization()
-            routing { productRoutes(FakeProductRepository()) }
+            configureSecurity()
+            routing {
+                authRoutes(userRepo)
+                productRoutes(productRepo)
+            }
         }
 
-        val response = client.delete("/products/12345")
+        val token = login(client, "admin@mail.com", "123456")
+
+        val response = client.delete("/products/12345") {
+            header(HttpHeaders.Authorization, "Bearer $token")
+        }
+
         assertEquals(HttpStatusCode.NotFound, response.status)
     }
+
 }
